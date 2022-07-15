d49ed57848042c90cf391918137900ab120758b2
Herman Streltsov <mail@streltsov.dev>
2022-07-15 14:02:23 +0300

    Create function splitByHeading

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,19 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+import Data.String.Regex (Regex, split)
+import Data.String.Regex.Flags (global)
+import Data.String.Regex.Unsafe (unsafeRegex)
+
+h2Regex :: String
+h2Regex = "((?<!=)={2}\\w+={2}(?!=))"
+
+pattern :: Regex
+pattern = unsafeRegex h2Regex global
+
+splitByHeading :: String -> Array String
+splitByHeading = split pattern
+
 main :: Effect Unit
 main = do
   log "🍝"
